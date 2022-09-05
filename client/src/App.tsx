265d80ad0f868d86cff1602be7ce448faa8ebed9
x4132 <31595285+x4132@users.noreply.github.com>
2022-09-04 20:42:13 -0700

    basic login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
+import { Form, Button } from "react-bootstrap"
 import "./scss/main.scss"
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-center">NETSI BASIC SERVER</h1>
+      <h1 className="text-center">NETSI</h1>
+
+      <div className={`d-flex flex-column justify-content-center align-items-center`} >
+        <div className={`mt-2`} >
+          <h2>Login:</h2>
+        </div>
+        <div className={`mt-2`} >
+          <Form.Control type="text" placeholder="Username" />
+        </div>
+        <div className={`mt-2`} >
+          <Form.Control type="password" placeholder="Password" />
+        </div>
+        <div className={`mt-2`} >
+          <Button variant="primary" >Login</Button>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Webshell from './Webshell'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="webshell" element={<Webshell />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
