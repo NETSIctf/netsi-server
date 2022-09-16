d1650d45b6b8117f1dec609448178b11cf5d0bfd
x4132 <31595285+x4132@users.noreply.github.com>
2022-09-15 17:59:43 -0700

    efficientize checkLoginNavigate()

--- client/src/Webshell.tsx
+++ client/src/Webshell.tsx
@@ -1,14 +1,9 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { checkLoginNavigate } from "./components/LoginChecks";
 import "./scss/main.scss";
 
 export default function Webshell() {
-    const navigate = useNavigate();
-
-    useEffect(() => { // runs on first render
-        checkLoginNavigate();
-    }, [])
+    checkLoginNavigate();
 
     return (
         <div>
--- client/src/components/LoginChecks.tsx
+++ client/src/components/LoginChecks.tsx
@@ -1,17 +1,18 @@
 import axios from "axios";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function checkLoginNavigate() {
-  // checks if the user is logged in. If they are, it does nothing. If they are not, it redirects them to the login page.
+export function checkLoginNavigate() {// checks if the user is logged in. If they are, it does nothing. If they are not, it redirects them to the login page.
   const navigate = useNavigate();
-
-  axios.get("/api/login").then(resolve => {
-    if (resolve.status !== 200) {
+  useEffect(() => {
+    axios.get("/api/login").then(resolve => {
+      if (resolve.status !== 200) {
+        navigate("/login");
+      }
+    }).catch(() => {
       navigate("/login");
-    }
-  }).catch(() => {
-    navigate("/login");
-  })
+    })
+  }, [])
 }
 
 export async function checkLogin() {
