4c1e2695429f801ba30601a9331bd3e27516c26d
x4132 <31595285+x4132@users.noreply.github.com>
2022-09-17 15:58:59 -0700

    webhooks only when production

--- server/utils/webhookMessage.ts
+++ server/utils/webhookMessage.ts
@@ -7,13 +7,18 @@ import axios from "axios";
  * @arg color https://www.spycolor.com/
  */
 export default function webhookMessage(title: String, description: String, color: number) {
-    const data = {
-        embeds: [{
-            title: title,
-            description: description,
-            color: color
-        }]
+    if (process.env.NODE_ENV === "production") {
+        const data = {
+            embeds: [{
+                title: title,
+                description: description,
+                color: color
+            }]
+        }
+    
+        console.log(`WebhookMessage: ${title}/${description}`);
+        axios.post("https://discord.com/api/webhooks/1016905904093925406/hQpKUm3elqbBKw7XIipjcODkVtwshuOiDfbORhGNIUUe9OwTRpqCp24Pv5UI0NVU9Giv", data)
+    } else {
+        console.log(`Simulated WebhookMessage: ${title}/${description}`)
     }
-
-    axios.post("https://discord.com/api/webhooks/1016905904093925406/hQpKUm3elqbBKw7XIipjcODkVtwshuOiDfbORhGNIUUe9OwTRpqCp24Pv5UI0NVU9Giv", data)
 }
