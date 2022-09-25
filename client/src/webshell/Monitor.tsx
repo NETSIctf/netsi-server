9ecf4812b33b45e70875acb988c1d54723bed299
x4132 <31595285+x4132@users.noreply.github.com>
2022-09-24 23:19:09 -0700

    commands

--- client/src/webshell/Monitor.tsx
+++ client/src/webshell/Monitor.tsx
@@ -5,65 +5,126 @@ import io from "socket.io-client";
 
 const socket = io({ autoConnect: false });
 
-type monDataInitType = { data: string, headers: any };
+type monDataType = { data: string, headers: any };
+
+type monDataOptions = {
+    showHeaders: boolean
+}
 
 export default function Monitor() {
     const [termData, setTermData] = useState<(TerminalOutput | TerminalInput)[]>([]);
 
-    const [options, setOptions] = useState();
+    const [options, setOptions_nomerge] = useState<monDataOptions>({ showHeaders: false });
+
+    function appendTermData(input: any, type: "output" | "input" = "output") {
+        if (type == "output") {
+            setTermData(prev => [...prev, <TerminalOutput key={prev.length}>{input}</TerminalOutput>]);
+        } else if (type == "input") {
+            setTermData(prev => [...prev, <TerminalInput key={prev.length}>{input}</TerminalInput>]);
+        }
+    }
+
+    function setOptions(options: Partial<monDataOptions>) {
+        setOptions_nomerge(prev => ({...prev, ...options}));
+    }
 
     useEffect(() => { // SOCKET INIT CODE
         socket.connect();
 
-        console.log(socket);
-
-        setTermData((arr) => [...arr, <TerminalOutput key={arr.length} >Connecting to socket...</TerminalOutput>]);
+        appendTermData("Connecting to socket...")
 
         socket.on("connect", () => {
-            setTermData((arr) => [...arr, <TerminalOutput key={arr.length} >Connected</TerminalOutput>]);
+            appendTermData("Connected");
 
             socket.emit("initMonitor");
         });
 
         socket.on('disconnect', function () {
-            setTermData((arr) => [...arr, <TerminalOutput key={arr.length} >Socket Disconnected</TerminalOutput>]);
+            appendTermData("Socket Disconnected");
         });
 
+        return () => {
+            socket.off("connect");
+            socket.off("disconnect");
+            socket.disconnect();
+
+            setTermData([]);
+        }
+    }, []);
+    useEffect(() => { // also socket stuff
         socket.on("monDataInit", (d: string) => {
-            var data: monDataInitType[] = JSON.parse(d);
+            var data: monDataType[] = JSON.parse(d);
             setTermData(prev => {
                 var toAdd: TerminalOutput = [];
                 data.forEach((i, count) => {
                     toAdd.push(<TerminalOutput key={prev.length + count}>
                         {i.data}<br />
-                        {JSON.stringify(i.headers)}
+                        {options.showHeaders ? JSON.stringify(i.headers) : null}
                     </TerminalOutput>)
                 })
-
 
                 return [...prev, toAdd];
             })
         });
 
         socket.on("monDataUpdate", (d: string) => {
-            var data: monDataInitType = JSON.parse(d);
-            setTermData(prev => [...prev, <TerminalOutput key={prev.length}>{data.data}<br />{JSON.stringify(data.headers)}</TerminalOutput>]);
+            var data: monDataType = JSON.parse(d);
+            console.log(options.showHeaders);
+            appendTermData(<>{data.data}<br />{options.showHeaders ? JSON.stringify(data.headers) : null}</>)
         })
 
         return () => {
-            socket.off("connect");
-            socket.off("disconnect");
             socket.off("monDataInit");
             socket.off("monDataUpdate");
-            socket.disconnect();
-
-            setTermData([]);
         }
-    }, []);
+    }, [options])
+
+    function commands(text: string) {
+        appendTermData(text, "input");
+
+        const args = text.split(" ");
+        switch (args[0]) {
+            case "help":
+                appendTermData(`
+set - Sets an Option
+get - Gets a value
+help - Shows this help page
+
+LIST OF OPTIONS:
+showheaders - true/false - Shows the header field or not.
+                `);
+                break;
+            case "set":
+                cmdOptionSet(args.slice(1, args.length));
+                break;
+            case "get":
+                break;
+            default:
+                appendTermData(`Unknown Command: ${args[0]}`);
+        }
+    }
+
+    function cmdOptionSet(args: string[]) {
+        switch (args[0]) {
+            case "showheaders":
+                if (args[1] == "true") {
+                    setOptions({showHeaders: true});
+                } else if (args[1] == "false") {
+                    setOptions({showHeaders: false});
+                } else {
+                    appendTermData(`Recieved Invalid Input for showheaders: ${args[1]}`);
+                    break;
+                }
+                appendTermData(`showheaders: ${args[1]}`);
+                break;
+            default:
+                appendTermData(`setOption: unknown property ${args[0]}`);
+        }
+    }
 
     return (
         <div>
-            <Terminal colorMode={ColorMode.Dark} prompt={`$`} onInput={() => { }} >
+            <Terminal colorMode={ColorMode.Dark} prompt={`$`} onInput={(text) => commands(text)} >
                 {termData}
             </Terminal>
         </div>
