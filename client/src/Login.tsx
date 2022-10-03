341d2452e615effbab5cc13d16f0b8e2c493a55f
TheArchons <progamer9138@gmail.com>
2022-10-03 15:56:30 -0700

    test push 2
    check if GRW webhooks are working

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { checkLoginNavigate } from "./components/LoginChecks";
 
 function Login() {
   const [username, setUsername] = useState("");
