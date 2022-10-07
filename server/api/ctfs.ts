81b20a827d1fbf1e30a62bfd286a8dacac9ea231
x4132 <31595285+x4132@users.noreply.github.com>
2022-10-07 11:44:47 -0700

    remove unnecessary name check

--- server/api/ctfs.ts
+++ server/api/ctfs.ts
@@ -68,13 +68,6 @@ export default function ctf() {
     router.post("/add", (req, res) => {
         // adds a new ctf
         if (req.check_auth("admin")) {
-            // check if name is valid (only contains letters and numbers)
-            if (req.body.name.match(/^[a-zA-Z0-9]+$/) === null) {
-                res.status(400);
-                res.end("Invalid name");
-                return;
-            }
-
             db.run("INSERT INTO ctfs (name, description, start, end) VALUES (?, ?, ?, ?)", [req.body.name, req.body.description, req.body.start, req.body.end], function (err) {
                 if (err) {
                     if (err.message.includes("UNIQUE constraint failed")) {
