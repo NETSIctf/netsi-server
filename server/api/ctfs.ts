84e8a96c16268ada492b68f3a815bf444c3b170b
TheArchons <progamer9138@gmail.com>
2022-10-10 13:29:18 -0700

    add columns without having to reset the db
    To add a new column to a table, add the column to the columns array.
    
    If the column has to be UNIQUE, you will have to reset the DB or manually add the UNIQUE constraint.
    
    If the column has the NOT NULL constraint, you will have to set a default value, manually add the constraint and fill the values, or reset the DB.

--- server/api/ctfs.ts
+++ server/api/ctfs.ts
@@ -12,38 +12,71 @@ const db = new sqlite3.Database("ctf.db", (err) => {
 const maxNameLength = 64;
 const maxDescriptionLength = 1024;
 
-// create ctfs table if it doesn't exist
-db.run(`CREATE TABLE IF NOT EXISTS ctfs( 
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE CHECK(length(name) < ${maxNameLength + 1}),
-    description TEXT NOT NULL CHECK(length(description) < ${maxDescriptionLength + 1}),
-    start DATE NOT NULL,
-    end DATE NOT NULL,
-    members TEXT)`,
-    (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log("Initialized ctf DB");
-    }
-);
+/*
+  To add a new column to a table, add the column to the columns array.
+  If the column has to be UNIQUE, you will have to reset the DB or manually add the UNIQUE constraint.
+  If the column has the NOT NULL constraint, you will have to set a default value, manually add the constraint and fill the values, or reset the DB.
+ */
 
-// add challenges table if it doesn't exist
-db.run(`CREATE TABLE IF NOT EXISTS challenges(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ctf_id INTEGER NOT NULL,
-    name TEXT NOT NULL CHECK(length(name) < ${maxNameLength + 1}),
-    description TEXT NOT NULL CHECK(length(description) < ${maxDescriptionLength + 1}),
-    points INTEGER NOT NULL CHECK(points >= 0),
-    solved_by TEXT,
-    FOREIGN KEY (ctf_id) REFERENCES ctfs(id))`,
-    (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log("Initialized challenges DB");
+function createCtfsTable() {
+    // create ctfs table if it doesn't exist
+    db.run(`CREATE TABLE IF NOT EXISTS ctfs(id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE CHECK(length(name) < ${maxNameLength +1}))`);
+
+    let alter: string = `ALTER TABLE ctfs ADD COLUMN`
+
+    let columns: string[] = [
+        `description TEXT NOT NULL CHECK(length(description) < ${maxDescriptionLength + 1})`,
+        `start DATE NOT NULL DEFAULT DATE('now')`,
+        `end DATE NOT NULL DEFAULT DATE('now')`,
+        `members TEXT`
+    ]
+
+    for (let column of columns) {
+        db.run(`${alter} ${column}`, (err) => {
+            if (err) {
+                if (err.message.includes("duplicate column name")) {
+                    return;
+                }
+                console.error(err.message);
+            }
+        });
     }
-);
+}
+
+function createChallengesTable() {
+    // create challenges table if it doesn't exist
+    db.run(`CREATE TABLE IF NOT EXISTS challenges(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT NOT NULL UNIQUE CHECK(length(name) < ${maxNameLength + 1}))`);
+
+    let alter: string = `ALTER TABLE challenges ADD COLUMN`
+
+    let columns = [
+        `ctf_id INTEGER NOT NULL`,
+        `description TEXT NOT NULL CHECK(length(description) < ${maxDescriptionLength + 1}) DEFAULT "default"`,
+        `points INTEGER NOT NULL CHECK(points >= 0) DEFAULT 0`,
+        `solved_by TEXT`
+    ]
+
+    for (let column of columns) {
+        db.run(`${alter} ${column};`, (err) => {
+            if (err) {
+                if (err.message.includes("duplicate column name")) {
+                    return;
+                }
+                console.error(err.message, column);
+            }
+        });
+    }
+
+    // create foreign key
+    db.run(`ALTER TABLE challenges ADD FOREIGN KEY (ctf_id) REFERENCES ctfs(id)`, () => {
+        // ignore errors because the foreign key might already exist
+    });
+}
+
+createCtfsTable();
+createChallengesTable();
 
 export default function ctf() {
     const router = Router();
