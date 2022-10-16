fc85fcd7b8fe220c386dd4cbae5e8d77e7bc144d
TheArchons <progamer9138@gmail.com>
2022-10-16 12:35:18 -0700

    check login + hide editor for non admins

--- client/src/ctfs/ChallengeWriteup.tsx
+++ client/src/ctfs/ChallengeWriteup.tsx
@@ -1,31 +1,49 @@
+import { checkAdmin, checkLoginNavigate } from "../components/LoginChecks";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import MDEditor from '@uiw/react-md-editor';
 import { Button } from "react-bootstrap";
 
 
 export default function ChallengeWriteup() {
+  checkLoginNavigate();
   const navigate = useNavigate();
   const ctfName = decodeURIComponent(new URLSearchParams(useLocation().search).get("title") as string); // name of ctf from query
   const challengeName = decodeURIComponent(new URLSearchParams(useLocation().search).get("challenge") as string); // name of challenge from query
 
-  const [writeup, setWriteup] = useState(`# ${challengeName} Writeup`);
+  const [writeup, setWriteup] = useState(`# ${ctfName} - ${challengeName} Writeup`);
+  const [isAdmin , setAdmin] = useState(false);
+
+  useEffect(() => {
+    checkAdmin([setAdmin]);
+  })
 
   return (
     <div>
-      <h1 className="text-center">{ctfName} - {challengeName}</h1>
-      <MDEditor
-        height={500}
-        value={writeup}
-        onChange={(value) => setWriteup(value as string)}
-      />
-      <div className={`text-center mt-2`}>
-        <Button variant="success">Save</Button>
-      </div>
-      <div className={`mt-2 text-center`} >
-        <Button variant="secondary" onClick={() => navigate(`/ctfs/view?title=${encodeURIComponent(ctfName)}`)}>Cancel</Button>
-      </div>
+      { isAdmin ?
+        /* If the user is an admin, give them the editor. Else, only show them the writeup */
+        <div>
+          <MDEditor
+            height={500}
+            value={writeup}
+            onChange={(value) => setWriteup(value as string)}
+          />
+          <div className={`text-center mt-2`}>
+            <Button variant="success">Save</Button>
+          </div>
+          <div className={`mt-2 text-center`} >
+            <Button variant="secondary" onClick={() => navigate(`/ctfs/view?title=${encodeURIComponent(ctfName)}`)}>Cancel</Button>
+          </div>
+        </div>
+
+        : <div>
+          <MDEditor.Markdown source={writeup} />
+          <div className={`mt-2 text-center`} >
+            <Button variant="secondary" onClick={() => navigate(`/ctfs/view?title=${encodeURIComponent(ctfName)}`)}>Return to ctf</Button>
+          </div>
+        </div>
+      }
     </div>
   )
 }
