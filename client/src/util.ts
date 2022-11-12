203aaa13e42b37c3f782e44f4edd87592a1fed3b
x4132 <31595285+x4132@users.noreply.github.com>
2022-11-11 21:18:32 -0800

    purge cache

--- client/src/util.ts
+++
@@ -1,9 +0,0 @@
-export function parseCookie(str: string) {
-    return str
-        .split(';')
-        .map(v => v.split('='))
-        .reduce((acc: any, v: any) => {
-            acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-            return acc;
-        }, {});
-}
