29416a148deb9a5153c5ddfb97aef3cc072ef7fd
x4132 <31595285+x4132@users.noreply.github.com>
2022-11-23 20:40:46 -0800

    a

--- client/src/ctfs/View_new.tsx
+++ client/src/ctfs/View_new.tsx
@@ -1,5 +1,8 @@
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { checkLoginNavigate } from "../components/LoginChecks";
+
+import { Settings } from "react-feather";
 
 export default function View() {
     const params = useParams();
@@ -7,19 +10,29 @@ export default function View() {
 
     checkLoginNavigate();
 
-    function deleteCTF() {}
+    function deleteCTF() { }
 
-    function addMember() {}
+    function addMember() { }
 
-    function deleteMember() {}
+    function deleteMember() { }
 
-    function solve() {}
+    function solve() { }
 
-    function deleteChallenge() {}
+    function deleteChallenge() { }
 
     return (
-        <>
-            {JSON.stringify(params)}
-        </>
+        <div className={`p-3 d-flex flex-column`} >
+            <div className="d-flex align-items-center" >
+                <div className="d-flex flex-column" >
+                    <h1 className="d-block" >ctf/{ctfName}</h1>
+                </div>
+
+                <div className="ms-auto" >
+                    <Button variant="outline-light" ><Settings className="d-block" size="18" /></Button>
+                </div>
+            </div>
+
+            <h6 className="d-block" >Filler_StartDate -&gt; Filler_EndDate</h6>
+        </div>
     );
 }
