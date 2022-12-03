e2189351c45acd69916f2362bd83d40ccbf335b4
TheArchons <progamer9138@gmail.com>
2022-12-03 11:25:55 -0800

    setAdded after ctfData.members is loaded
    there's probably a better way to do this, but it works.

--- client/src/ctfs/View/MembersList.tsx
+++ client/src/ctfs/View/MembersList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import {useContext, useEffect, useState} from "react"
 import { MinusSquare, PlusSquare, RefreshCw } from "react-feather"
 import { getUsername } from "../../util";
 import { CTFContext } from "./View"
@@ -6,11 +6,13 @@ import { CTFContext } from "./View"
 export default function MembersList() {
     const [ctfData, setCtfData] = useContext(CTFContext);
     const [changing, setChanging] = useState(false);
-    const [added, setAdded] = useState(ctfData.members.includes(getUsername()));
+    const [added, setAdded] = useState(false);
 
-    console.log(ctfData.members.includes(getUsername()));
+    let members = ctfData.members.map((name) => <Member name={name} key={name} />);
 
-    var members = ctfData.members.map((name) => <Member name={name} key={name} />);
+    useEffect(() => {
+        setAdded(ctfData.members.includes(getUsername()));
+    }, [ctfData.members]);
 
     function addMember() {
         setChanging(true);
@@ -40,6 +42,7 @@ export default function MembersList() {
         <div className={`p-0 d-flex flex-column position-absolute top-50 end-0 translate-middle-y rounded-start border border-end-0 h-50`} >
             <div className="d-flex align-items-center border-bottom p-2" >
                 <h1 className="me-5" >Members</h1>
+                <h1>{added}</h1>
                 {!changing ?
                     !added ?
                         <PlusSquare className="text-success cursor-pointer" size="35" onClick={() => addMember()} /> : // User added or not
